1105b0158bee758889ca26b7745903fed2604b37
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-04-26 21:27:00 -0700

    Procesos de asignacion de contraseña y confirmacion de cuenta

---
+++ client/src/components/ui/Login/Procesos/CrearCuenta/ConfirmacionCuenta.jsx
@@ -0,0 +1,134 @@
+import user1 from "../../../../../../public/pictures/userGuest.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  fa1,
+  fa2,
+  fa3,
+  fa4,
+  faCircle,
+} from "@fortawesome/free-solid-svg-icons";
+
+const ConfirmacionCuenta = () => {
+  return (
+    <div className="flex flex-col h-screen justify-center items-center">
+      <div className="h-[470px] max-w-[765px] flex flex-col rounded-xl ring-1 ring-slate-200 bg-white bg-clip-border text-gray-700 shadow-md mx-auto">
+        <div className="relative max-w-[110px] min-w-[110px] mx-auto -mt-20 grid h-28 place-items-center overflow-hidden rounded-full bg-white bg-clip-border shadow-lg">
+          <img src={user1} alt="" className="logo1 absolute h-full" />
+        </div>
+        <h1 className="mx-auto p-5 text-rose-400 text-3xl">
+          Crear cuenta nueva
+        </h1>
+        <div className="grid grid-cols-[50px_auto_20px_50px_auto_20px_50px_auto_20px_50px_auto] place-items-center px-5 mx-auto">
+          <span className="">
+            <FontAwesomeIcon
+              style={{ fontSize: "16px", color: "#FFFFFF" }}
+              icon={fa1}
+              className="relative left-6 bottom-2"
+            />
+            <FontAwesomeIcon
+              style={{ fontSize: "36px" }}
+              icon={faCircle}
+            ></FontAwesomeIcon>
+          </span>
+          <a
+            href=""
+            style={{
+              fontFamily: "ABeeZee",
+            }}
+            className="text-base mx-2"
+          >
+            Información básica
+          </a>
+          <div class="flex-grow border-t border-gray-400 pl-4 mx-0"></div>
+          <span className="">
+            <FontAwesomeIcon
+              style={{ fontSize: "16px", color: "#FFFFFF" }}
+              icon={fa2}
+              className="relative left-6 bottom-2"
+            />
+            <FontAwesomeIcon
+              style={{ fontSize: "36px" }}
+              icon={faCircle}
+            ></FontAwesomeIcon>
+          </span>
+          <a
+            href=""
+            style={{
+              fontFamily: "ABeeZee",
+            }}
+            className="text-base mx-1"
+          >
+            Patologías
+          </a>
+          <div class="flex-grow border-t border-gray-400 pl-4 mx-0"></div>
+          <span className="">
+            <FontAwesomeIcon
+              style={{ fontSize: "16px", color: "#FFFFFF" }}
+              icon={fa3}
+              className="relative left-6 bottom-2"
+            />
+            <FontAwesomeIcon
+              style={{ fontSize: "36px" }}
+              icon={faCircle}
+            ></FontAwesomeIcon>
+          </span>
+          <a
+            href=""
+            style={{
+              fontFamily: "ABeeZee",
+            }}
+            className="text-base mx-1"
+          >
+            Contraseña
+          </a>
+          <div class="flex-grow border-t border-gray-400 pl-4 mx-0"></div>
+          <span className="">
+            <FontAwesomeIcon
+              style={{ fontSize: "16px", color: "#FFFFFF" }}
+              icon={fa4}
+              className="relative left-6 bottom-2"
+            />
+            <FontAwesomeIcon
+              style={{ fontSize: "36px" }}
+              icon={faCircle}
+            ></FontAwesomeIcon>
+          </span>
+          <a
+            href=""
+            style={{
+              fontFamily: "ABeeZee",
+            }}
+            className="text-base mx-1"
+          >
+            Confirmación
+          </a>
+        </div>
+        <div className="grid grid-cols-1 mx-auto my-auto place-items-center">
+          <h2 className="w-2/3 text-center text-lg text-rose-400 my-3">
+            ¡Tu contraseña ha sido creada de forma exitosa!
+          </h2>
+          <h2 className="w-2/3 text-center text-lg text-teal-700 mt-1 mb-3">
+            El proceso ha terminado y ahora podrás iniciar sesión cuando
+            quieras.
+          </h2>
+          <h2 className="w-2/3 text-center text-sm">
+            Recuerda que al tener cuenta, no solo sirve para puedas saber de ti,
+            también sirve para que puedas guardar tus servicios favoritos,
+            realizar compras, agregar a favoritos y sobre todo, para compartir
+            tus experiencias con clientes como tú.
+          </h2>
+        </div>
+        <div className="grid grid-cols-1 my-auto place-items-end">
+          <button
+            aria-label="Regresar al inicio"
+            className="w-52 bg-rose-400 text-white text-xl rounded-full px-4 py-2 mr-8 hover:bg-red-200"
+          >
+            Regresar al inicio
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmacionCuenta;
---
+++ client/src/components/ui/Login/Procesos/CrearCuenta/Contrasena.jsx
@@ -0,0 +1,198 @@
+import user1 from "../../../../../../public/pictures/userGuest.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  fa1,
+  fa2,
+  fa3,
+  fa4,
+  faCircle,
+  faAngleLeft,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Contrasena = () => {
+  return (
+    <div className="flex flex-col h-screen justify-center items-center">
+      <div className="h-[470px] max-w-[765px] flex flex-col rounded-xl ring-1 ring-slate-200 bg-white bg-clip-border text-gray-700 shadow-md mx-auto">
+        <div className="relative max-w-[110px] min-w-[110px] mx-auto -mt-20 grid h-28 place-items-center overflow-hidden rounded-full bg-white bg-clip-border shadow-lg">
+          <img src={user1} alt="" className="logo1 absolute h-full" />
+        </div>
+        <h1 className="mx-auto p-5 text-rose-400 text-3xl">
+          Crear cuenta nueva
+        </h1>
+        <div className="grid grid-cols-[50px_auto_20px_50px_auto_20px_50px_auto_20px_50px_auto] place-items-center px-5 mx-auto">
+          <span className="">
+            <FontAwesomeIcon
+              style={{ fontSize: "16px", color: "#FFFFFF" }}
+              icon={fa1}
+              className="relative left-6 bottom-2"
+            />
+            <FontAwesomeIcon
+              style={{ fontSize: "36px" }}
+              icon={faCircle}
+            ></FontAwesomeIcon>
+          </span>
+          <a
+            href=""
+            style={{
+              fontFamily: "ABeeZee",
+            }}
+            className="text-base mx-2"
+          >
+            Información Básica
+          </a>
+          <div class="flex-grow border-t border-gray-400 pl-4 mx-0"></div>
+          <span className="">
+            <FontAwesomeIcon
+              style={{ fontSize: "16px", color: "#FFFFFF" }}
+              icon={fa2}
+              className="relative left-6 bottom-2"
+            />
+            <FontAwesomeIcon
+              style={{ fontSize: "36px" }}
+              icon={faCircle}
+            ></FontAwesomeIcon>
+          </span>
+          <a
+            href=""
+            style={{
+              fontFamily: "ABeeZee",
+            }}
+            className="text-base mx-1"
+          >
+            Patologías
+          </a>
+          <div class="flex-grow border-t border-gray-400 pl-4 mx-0"></div>
+          <span className="">
+            <FontAwesomeIcon
+              style={{ fontSize: "16px", color: "#FFFFFF" }}
+              icon={fa3}
+              className="relative left-6 bottom-2"
+            />
+            <FontAwesomeIcon
+              style={{ fontSize: "36px" }}
+              icon={faCircle}
+            ></FontAwesomeIcon>
+          </span>
+          <a
+            href=""
+            style={{
+              fontFamily: "ABeeZee",
+            }}
+            className="text-base mx-1"
+          >
+            Contraseña
+          </a>
+          <div class="flex-grow border-t border-gray-400 pl-4 mx-0"></div>
+          <span className="">
+            <FontAwesomeIcon
+              style={{ fontSize: "16px", color: "#FFFFFF" }}
+              icon={fa4}
+              className="relative left-6 bottom-2"
+            />
+            <FontAwesomeIcon
+              style={{ fontSize: "36px" }}
+              icon={faCircle}
+            ></FontAwesomeIcon>
+          </span>
+          <a
+            href=""
+            style={{
+              fontFamily: "ABeeZee",
+            }}
+            className="text-base mx-1"
+          >
+            Confirmación
+          </a>
+        </div>
+        <div className="absolute mt-5">
+          <a className="ml-3">
+            <FontAwesomeIcon style={{ fontSize: "22px" }} icon={faAngleLeft} />{" "}
+            Regresar
+          </a>
+        </div>
+        <div className="grid grid-cols-2 mt-5 mb-2">
+          <div className="ml-10 mr-4">
+            <h3 className="mx-auto mb-3 text-red-500">
+              Requisitos de la contraseña
+            </h3>
+            <h4 className="text-xs"> • Al menos 8 caracteres</h4>
+            <h4 className="text-xs"> • Una letra mayúscula</h4>
+            <h4 className="text-xs"> • Una letra minúscula</h4>
+            <h4 className="text-xs"> • Un número</h4>
+            <h4 className="text-xs"> • Al menos un carácter especial</h4>
+            <h4 className="text-xs">
+              {" "}
+              • La contraseña no debe coincidir con su ID de correo electrónico
+              y debe distinguir entre mayúsculas y minúsculas
+            </h4>
+          </div>
+          <div>
+            <form action="" className="grid grid-cols-1 place-content-start">
+              <div>
+                <label htmlFor="">Nueva contraseña</label>
+                <label htmlFor="" className="text-red-800">
+                  *
+                </label>
+              </div>
+              <input
+                id="email"
+                type="text"
+                placeholder="Ingresa tu nueva contraseña"
+                className="bg-slate-200 rounded-full w-[365px] mb-3 mt-2 py-3 focus:outline-none focus:ring-1 focus:ring-rose-400 focus:border-transparent px-6"
+              />
+              <div>
+                <label htmlFor="">Confirmar contraseña</label>
+                <label htmlFor="" className="text-red-800">
+                  *
+                </label>
+              </div>
+              <input
+                id="email"
+                type="text"
+                placeholder="Vuelve a ingresar tu nueva contraseña"
+                className="bg-slate-200 rounded-full w-[365px] mb-3 mt-2 py-3 focus:outline-none focus:ring-1 focus:ring-rose-400 focus:border-transparent px-6"
+              />
+            </form>
+          </div>
+        </div>
+        <div className="grid grid-cols-[5%_auto] ml-8">
+          <input type="checkbox" name="acept" id="state" value={true} />
+          <h4 className="text-xs">
+            Al hacer click en "Registrarme", aceptas nuestras{" "}
+            <a href="" className="text-blue-600">
+              Condiciones
+            </a>
+            , la{" "}
+            <a href="" className="text-blue-600">
+              Politica de Privacidad
+            </a>{" "}
+            y la{" "}
+            <a href="" className="text-blue-600">
+              Politica de Cookies
+            </a>
+          </h4>
+        </div>
+        <div className="grid grid-cols-2 my-auto">
+          <div className="grid place-content-start ml-8">
+            <button
+              aria-label="Cancelar"
+              className="bg-white text-rose-400 text-xl rounded-full px-4 py-2 mx-auto hover:bg-red-50 ring-2 ring-rose-400"
+            >
+              Cancelar
+            </button>
+          </div>
+          <div className="grid place-content-end mr-8">
+            <button
+              aria-label="Continuar"
+              className="bg-rose-400 text-white text-xl rounded-full px-4 py-2 mx-auto hover:bg-red-200"
+            >
+              Continuar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contrasena;
