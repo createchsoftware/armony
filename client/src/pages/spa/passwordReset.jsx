ae6a089531af163f565e2b6d3d956c385b2e51cf
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-04-26 18:23:03 -0700

    Pagina del proceso de cambio de contraseña

--- client/src/pages/spa/passwordReset.jsx
+++ client/src/pages/spa/passwordReset.jsx
@@ -1,15 +1,15 @@
 import Navbar from "../../components/ui/Navbar.jsx";
-import Password1 from "../../components/ui/Login/Procesos/Contraseña/CuadroDatosRecuperacion.jsx";
+import Recuperacion from "../../components/ui/Login/Procesos/Contraseña/Confirmacion.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-const PasswordReset = () => {
+const passwordReset = () => {
   return (
     <>
       <Navbar />
-      <Password1 />
+      <Recuperacion />
     </>
   );
 };
 
-export default PasswordReset;
+export default passwordReset;
