3082f33f50f3078b853596a510db053c3ad26b7a
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-04-26 21:11:14 -0700

    Añadidas rutas de procesos de sesion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,29 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import Spa from "./pages/spa";
 import Tienda from "./pages/Tienda";
 import Error from "./pages/Error";
 import PasswordReset from "./pages/spa/passwordReset";
+import SingUp from "./pages/spa/signUp";
 import Favicon from "react-favicon";
- 
+
 function App() {
-    return (
-        <>
-        <Favicon url="/favicon.ico" />
-            <Router>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/spa" element={<Spa />} />
-                    <Route path="/spa/tienda" element={<Tienda />} />
-                    <Route path="/spa/resetPassword" element={<PasswordReset />} />
-                    <Route path="*" element={<Error />} />
-                </Routes>
-            </Router>
-        </>
-    );
+  return (
+    <>
+      <Favicon url="/favicon.ico" />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/spa" element={<Spa />} />
+          <Route path="/spa/tienda" element={<Tienda />} />
+          <Route path="/spa/resetPassword" element={<PasswordReset />} />
+          <Route path="/spa/signUp" element={<SingUp />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
- 
+
 export default App;
