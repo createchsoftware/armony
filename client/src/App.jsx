2a3fe25c921c61abc63a333c780fd9202802713b
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-01 23:15:57 -0700

    Añadidas rutas de servicios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,4 @@
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import Spa from "./pages/spa";
 import Tienda from "./pages/Tienda";
@@ -16,37 +12,44 @@ import Tarjetas from "./pages/perfil/Tarjetas";
 import Error from "./pages/Error";
 import PasswordReset from "./pages/spa/passwordReset";
 import SignUp from "./pages/spa/signUp";
+import ServiciosSpa from "./pages/spa/spaServicios";
+import ServiciosEstetica from "./pages/spa/esteticaServicios";
 import Favoritos from "./pages/perfil/ListaDeseo";
 import Test from "./components/ui/Test";
 import Favicon from "react-favicon";
- 
-function App() {
-    return (
-        <>
-        <Favicon url="/favicon.ico" />
-            <Router>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/spa" element={<Spa />} />
-                    <Route path="/spa/tienda" element={<Tienda />} />
-                    <Route path="/spa/resetPassword" element={<PasswordReset />} />
-                    <Route path="/spa/signUp" element={<SignUp />} />
-                    <Route path="/perfil" element={<Perfil />} />
-                    <Route path="/perfil/informacion" element={<InformacionPersonal />} />
-                    <Route path="/perfil/seguridad" element={<Seguridad />} />
-                    <Route path="/perfil/monedero" element={<Monedero />} />
-                    <Route path="/perfil/direcciones" element={<Direcciones />} />
-                    <Route path="/perfil/suscripciones" element={<Suscripciones />} />
-                    <Route path="/perfil/tarjetas" element={<Tarjetas />} />
-                    <Route path="/favoritos" element={<Favoritos/>} />
-                    <Route path="*" element={<Error />} />
 
-                    {/* Tests */}
-                    <Route path="/test" element={<Test />} />
-                </Routes>
-            </Router>
-        </>
-    );
+function App() {
+  return (
+    <>
+      <Favicon url="/favicon.ico" />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/spa" element={<Spa />} />
+          <Route path="/spa/tienda" element={<Tienda />} />
+          <Route path="/spa/resetPassword" element={<PasswordReset />} />
+          <Route path="/spa/signUp" element={<SignUp />} />
+          <Route path="/spa/serviciosSpa" element={<ServiciosSpa />} />
+          <Route
+            path="/spa/serviciosEstetica"
+            element={<ServiciosEstetica />}
+          />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/perfil/informacion" element={<InformacionPersonal />} />
+          <Route path="/perfil/seguridad" element={<Seguridad />} />
+          <Route path="/perfil/monedero" element={<Monedero />} />
+          <Route path="/perfil/direcciones" element={<Direcciones />} />
+          <Route path="/perfil/suscripciones" element={<Suscripciones />} />
+          <Route path="/perfil/tarjetas" element={<Tarjetas />} />
+          <Route path="/favoritos" element={<Favoritos />} />
+          <Route path="*" element={<Error />} />
+
+          {/* Tests */}
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
- 
+
 export default App;
