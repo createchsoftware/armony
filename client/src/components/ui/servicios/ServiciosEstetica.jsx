21049809479a2dc86a4f4761a3821c4f182b7c6d
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-08 23:08:14 -0700

    Modificado para que se mapeen los servicios

--- client/src/components/ui/servicios/ServiciosEstetica.jsx
+++ client/src/components/ui/servicios/ServiciosEstetica.jsx
@@ -4,6 +4,36 @@ import Peluqueria from "../../../../public/pictures/peluqueria.png";
 import Unas from "../../../../public/pictures/unas.png";
 import Pedicura from "../../../../public/pictures/pedicura.png";
 
+var estetica = [
+  {
+    id: 1,
+    nombre: "Servicio de peluquería",
+    descripcion: "Espacio de transformación y renovación personal",
+    precio: "1,350.00",
+    img: Peluqueria,
+    rating: 5,
+    fav: true,
+  },
+  {
+    id: 2,
+    nombre: "Uñas",
+    descripcion: "Transformación de las manos elevando confianza",
+    precio: "1,200.00",
+    img: Unas,
+    rating: 5,
+    fav: true,
+  },
+  {
+    id: 3,
+    nombre: "Pedicura",
+    descripcion: "Tratamiento estético para los pies",
+    precio: "1,800.00",
+    img: Pedicura,
+    rating: 5,
+    fav: true,
+  },
+];
+
 const ServiciosEstetica = () => {
   return (
     <div>
@@ -19,30 +49,16 @@ const ServiciosEstetica = () => {
         </p>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-3 w-[90%] md:w-[60%] rounded-lg ring-4 ring-[#E2B3B7] mx-auto mb-10">
-        <Servicio
-          nombre={"Servicio de peluquería"}
-          descripcion={"Espacio de transformación y renovación personal"}
-          precio={"1,000.00"}
-          imagen={Peluqueria}
-          rating={5}
-          isFavorite={true}
-        />
-        <Servicio
-          nombre={"Uñas"}
-          descripcion={"Transformación de las manos elevando confianza"}
-          precio={"1,200.00"}
-          imagen={Unas}
-          rating={5}
-          isFavorite={true}
-        />
-        <Servicio
-          nombre={"Pedicura"}
-          descripcion={"Tratamiento estético para los pies"}
-          precio={"1,800.00"}
-          imagen={Pedicura}
-          rating={5}
-          isFavorite={true}
-        />
+        {estetica.map((servicio) => (
+          <Servicio
+            nombre={servicio.nombre}
+            descripcion={servicio.descripcion}
+            precio={servicio.precio}
+            imagen={servicio.img}
+            rating={servicio.rating}
+            isFavorite={servicio.fav}
+          />
+        ))}
       </div>
       <div className="w-full mx-auto my-16 text-center">
         <p className="text-4xl md:text-6xl w-3/4 md:w-full my-10 mx-auto text-center font-[ILoveGlitter] text-[#036C65]">
