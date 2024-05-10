1a0a21374b198f3ab361469290cc281b7d8ef36c
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-09 23:35:53 -0700

    Añadido ver descripción de la tarjeta de agendar

--- client/src/components/ui/TarjetaDeServicio.jsx
+++ client/src/components/ui/TarjetaDeServicio.jsx
@@ -3,6 +3,8 @@ import { Rating } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import { IoIosInformationCircle } from "react-icons/io";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
@@ -16,23 +18,41 @@ const StyledRating = styled(Rating)({
 });
 
 const TarjetaDeServicio = ({ servicio }) => {
-  const [vista, setVista] = useState("foto");
-  if (vista === "foto") {
-  }
+  const [vista, setVista] = useState(false);
+
+  const toogleVista = () => {
+    setVista(!vista);
+  };
 
   return (
     <div className="w-[17rem] mt-5">
-      <div className="w-[16rem] h-[18rem] mb-4 mx-auto">
-        <img
-          src={servicio.img}
-          alt=""
-          className="relative w-full h-full rounded-[2.25rem]"
+      <div className={vista ? "hidden" : "block"}>
+        <div className="w-[16rem] h-[18rem] mb-4 mt-1 mx-auto">
+          <img
+            src={servicio.img}
+            alt=""
+            className="relative w-full h-full rounded-[2.25rem]"
+          />
+        </div>
+        <IoIosInformationCircle
+          className="relative float-right -top-[18rem] right-5"
+          style={{ fontSize: "32px", color: "#A0A0A0" }}
+          onClick={toogleVista}
         />
       </div>
-      <IoIosInformationCircle
-        className="relative float-right -top-[18rem] right-5"
-        style={{ fontSize: "32px", color: "#A0A0A0" }}
-      />
+      <div className={vista ? "block" : "hidden"}>
+        <div className="w-[16rem] h-[18rem] mb-4 mt-1 mx-auto rounded-[2.25rem] bg-[#499382]">
+          <p className="grid pt-10 text-white text-center text-sm mx-5">
+            {servicio.descripcion}
+          </p>
+        </div>
+        <FontAwesomeIcon
+          style={{ fontSize: "22px" }}
+          icon={faAngleLeft}
+          onClick={toogleVista}
+          className="relative float-right -top-[18rem] right-5"
+        />
+      </div>
       <div className="w-[13rem] h-[10rem] mx-auto bg-rose-100 rounded-3xl ring-1 ring-rose-300 py-3">
         <Box
           className="float-right mr-2"
@@ -47,7 +67,12 @@ const TarjetaDeServicio = ({ servicio }) => {
             getLabelText={(value) => `${value} Heart${value !== 1 ? "s" : ""}`}
             precision={1}
             icon={<FavoriteIcon fontSize="inherit" />}
-            emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+            emptyIcon={
+              <FavoriteBorderIcon
+                style={{ color: "black" }}
+                fontSize="inherit"
+              />
+            }
           />
         </Box>
         <p className="text-lg font-bold pl-5 w-[80%]">{servicio.nombre}</p>
