c7827e8bddd11a0f390487b234eb7d58f10ecf3f
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-15 13:27:43 -0700

    Añadido el id como se muestra en el diseño

--- client/src/components/ui/Login/Procesos/CrearCuenta/ConfirmacionCuenta.jsx
+++ client/src/components/ui/Login/Procesos/CrearCuenta/ConfirmacionCuenta.jsx
@@ -1,12 +1,14 @@
 import user1 from "../../../../../../public/pictures/userGuest.png";
 import Pasos from "../../../PasosDeProcesos";
 
+const id = 1234567890;
+
 const ConfirmacionCuenta = () => {
   return (
     <div>
       <div className="md:h-20 h-[3.75rem] bg-white" />
       <div className="justify-center items-center mt-[6rem] mb-[6rem] md:mt-[5rem] md:mb-2 lg:mt-[8rem] lg:mb-4">
-        <div className="h-[31rem] w-[25rem] md:h-[28rem] lg:h-[29rem] md:w-[37rem] lg:w-[45rem] flex flex-col rounded-xl ring-1 ring-slate-200 bg-white bg-clip-border text-gray-700 shadow-md mx-auto">
+        <div className="h-[34rem] w-[25rem] md:h-[31rem] md:w-[37rem] lg:h-[33rem] lg:w-[45rem] flex flex-col rounded-xl ring-1 ring-slate-200 bg-white bg-clip-border text-gray-700 shadow-md mx-auto">
           <div className="relative w-[6rem] h-[6rem] lg:w-[7rem] lg:h-[7rem] mx-auto -mt-20 grid place-items-center overflow-hidden rounded-full bg-white bg-clip-border shadow-lg">
             <img src={user1} alt="" className="logo1 absolute h-full" />
           </div>
@@ -107,13 +109,16 @@ const ConfirmacionCuenta = () => {
           </a>
           </div>*/}
           <div className="grid grid-cols-1 mx-auto my-auto place-items-center">
-            <h2 className="w-5/6 md:w-2/3 text-center md:text-base lg:text-lg text-rose-400 my-3">
+            <h2 className="w-5/6 md:w-2/3 text-center md:text-base lg:text-lg text-[#EB5765] my-3">
               ¡Tu contraseña ha sido creada de forma exitosa!
             </h2>
-            <h2 className="w-5/6 md:w-2/3 text-center md:text-base lg:text-lg text-teal-700 mt-1 mb-3">
-              El proceso ha terminado y ahora podrás iniciar sesión cuando
-              quieras.
+            <h2 className="w-5/6 md:w-2/3 text-center md:text-base lg:text-lg text-[#056761] mt-1 mb-3">
+              El proceso ha terminado y ahora podrás iniciar sesión con una ID
+              única:
             </h2>
+            <div className="mb-3 bg-[#E8C3C6] rounded-full py-2 px-3">
+              <p className="text-[#EB5765] text-lg">#{id}</p>
+            </div>
             <h2 className="w-5/6 lg:w-2/3 text-center text-sm">
               Recuerda que al tener cuenta, no solo sirve para puedas saber de
               ti, también sirve para que puedas guardar tus servicios favoritos,
