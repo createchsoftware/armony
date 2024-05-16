5c9271d2d488fdc60a85851c68a36e103dc642c7
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-15 18:41:18 -0700

    Añadido que se pueda descargar el QR

--- client/src/components/ui/TicketServicio.jsx
+++ client/src/components/ui/TicketServicio.jsx
@@ -5,6 +5,26 @@ import { CiSaveDown2 } from "react-icons/ci";
 const TicketServicio = () => {
   var numTicket = 123456;
   var value = "lo que sea";
+
+  var onImageDownload = () => {
+    const svg = document.getElementById("QRCode");
+    const svgData = new XMLSerializer().serializeToString(svg);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      const pngFile = canvas.toDataURL("image/png");
+      const downloadLink = document.createElement("a");
+      downloadLink.download = "QRCode";
+      downloadLink.href = `${pngFile}`;
+      downloadLink.click();
+    };
+    img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+  };
+
   return (
     <>
       <div className="grid items-center content-center m-auto place-items-center font-bold">
@@ -23,6 +43,7 @@ const TicketServicio = () => {
             </h1>
             <div className="ring-4 ring-rose-200 mx-[10rem]">
               <QrCode
+                id="QRCode"
                 size={256}
                 style={{
                   height: "20rem",
@@ -46,7 +67,10 @@ const TicketServicio = () => {
                 />
               </button>
               {/*<button>Escanear QR </button>*/}
-              <button className="flex py-3 justify-center w-[10rem] h-[3rem] rounded-full shadow-[0_2px_10px_rgb(0,0,0,0.8)] text-sm font-[ABeeZee]">
+              <button
+                onClick={onImageDownload}
+                className="flex py-3 justify-center w-[10rem] h-[3rem] rounded-full shadow-[0_2px_10px_rgb(0,0,0,0.8)] text-sm font-[ABeeZee]"
+              >
                 Guardar QR{" "}
                 <CiSaveDown2
                   style={{
