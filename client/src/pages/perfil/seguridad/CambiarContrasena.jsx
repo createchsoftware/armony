a61f19595f50729dbaff8a82e1c10aac37324987
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-17 19:25:05 -0700

    Añadidas páginas de seguridad del perfil

---
+++ client/src/pages/perfil/seguridad/CambiarContrasena.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../../../components/ui/Navbar.jsx";
+import ValidarIdentidad from "../../../components/ui/perfil/ValidarIdentidad.jsx";
+import Contrasena from "../../../components/ui/perfil/CambiarContrasena.jsx";
+import Confirmacion from "../../../components/ui/perfil/ConfirmacionContrasena.jsx";
+import Footer from "../../../components/ui/FooterSecundario.jsx";
+
+const CambiarContrasena = () => {
+  return (
+    <>
+      <Navbar />
+      {/* FALTA RENDERIZAR PAGINAS */}
+      <Footer />
+    </>
+  );
+};
+
+export default CambiarContrasena;
