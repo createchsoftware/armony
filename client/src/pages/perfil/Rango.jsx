70d525b7c4f1a4d286ff6af72cce86c6b8741caf
Adrian Santelis <adrs170303@gmail.com>
2024-05-17 23:49:40 -0700

    Diseño arreglado en rango

--- client/src/pages/perfil/Rango.jsx
+++ client/src/pages/perfil/Rango.jsx
@@ -44,6 +44,29 @@ function Rango() {
     return (
         <LayoutPrincipal>
             <main className='grid gap-12 my-24'>
+                <section className='rounded-2xl mt-12 w-[60%] m-auto p-6 shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+                    <a className='flex items-baseline content-center text-sm gap-x-4' href="/spa"> <IoIosArrowBack className='' />
+                        Volver</a>
+                    <img className='w-32 m-auto my-6 -mt-24 rounded-full aspect-square' src="../../pictures/spa.png" alt="" />
+                    <div className='m-auto text-center '>
+                        <div className='flex items-center justify-center gap-4'>
+                            <h1 className='text-[#EB5765] font-semibold text-xl mb-2'>Nombre de Usuario</h1> <img className='w-8' src="../pictures/rango.png" alt="" />
+                        </div>
+                    </div>
+                </section>
+
+                <section className='w-[60%] m-auto'>
+                    <div className='rounded-2xl m-auto grid p-12 gap-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+                        <h2 className='text-[#036C65] text-lg'>Tu rango actual</h2>
+                        <p className='text-sm text-justify'>Este es tu rango actual. Recuerda que tu nivel será actualizado automáticamente cuando alcances los puntos suficientes para el siguiente nivel.</p>
+                        <div className='rounded-xl m-auto w-1/3 text-center bg-[#45B59C] mt-4 my-auto  p-3'>
+                            <div className='grid gap-2 px-2 py-6 bg-white rounded-lg'>
+                                <p>Rango Platino</p>
+                                <img className='w-24 m-auto' src={rangoPlatino} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
                 <section className='w-[60%] m-auto'>
                     <div className='rounded-2xl  m-auto grid gap-4 p-12 shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
