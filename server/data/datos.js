248e50c54a95a6ea67264ae16432cd912504b6ea
Julian Sandoval <76192811+Julian-Sandoval-x@users.noreply.github.com>
2024-05-18 23:44:27 -0700

    Conexion con API externa para imagenes y creacion de funcion para la carga de imagenes (Pendiente a probar)

--- server/data/datos.js
+++ server/data/datos.js
@@ -9,18 +9,23 @@ export const conexionDB = {
   PORT: process.env.DB_PORT,
   DATABASE: process.env.DB_NAME,
   PASSWORD: process.env.DB_PASSWORD,
-  USER: process.env.DB_USER
+  USER: process.env.DB_USER,
 };
 
 // Configuracion del servidor
 export const servidor = {
   SERVER_HOST: process.env.SERVER_HOST,
-  SERVER_PORT: process.env.SERVER_PORT
+  SERVER_PORT: process.env.SERVER_PORT,
 };
 
 // Configuracion de JWT
 export const jwt = {
   SECRET: process.env.JWT_SECRET,
   EXPIRATION: process.env.JWT_EXPIRATION,
-  COOKIE_EXPIRES: process.env.JWT_COOKIE_EXPIRES
+  COOKIE_EXPIRES: process.env.JWT_COOKIE_EXPIRES,
+};
+
+// API imagenes
+export const API_IMG = {
+  KEY_API_IMG: process.env.API_IMG,
 };
