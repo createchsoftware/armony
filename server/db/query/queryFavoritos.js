819c1fee010138a878bdf76fce7ab3cc0d8ff48f
Julian Sandoval <76192811+Julian-Sandoval-x@users.noreply.github.com>
2024-05-19 00:40:47 -0700

    se agrego CALL en querys faltantes y actualizacion de package.json

--- server/db/query/queryFavoritos.js
+++ server/db/query/queryFavoritos.js
@@ -1,54 +1,47 @@
 import mysql from "mysql2";
 
-
-export async function addfavorito(conexion,data){
-            try {
-                const call='CALL addFavorito(?,?,?)'
-                const query=mysql.format(call,[data.idCliente,data.IdProducto])
-                await conexion.query(query)
-             } catch(err){
-                console.log("Ha ocurrido un error al ejecutar el query: ",err)
-                throw err;
-                }
+export async function addfavorito(conexion, data) {
+  try {
+    const call = "CALL addFavorito(?,?,?)";
+    const query = mysql.format(call, [data.idCliente, data.IdProducto]);
+    await conexion.query(query);
+  } catch (err) {
+    console.log("Ha ocurrido un error al ejecutar el query: ", err);
+    throw err;
+  }
 }
 
-export async function delFavorito(conexion,data){
-   
-            try{
-                const call='CALL delFav(?,?)'
-                const query=mysql.format(call,[data.idCliente,data.IdProducto])
-                await conexion.query(query)
-                 
-                } catch(err){
-                    console.log("Ha ocurrido un error al ejecutar el query: ",err)
-                    throw err;
-                    }
+export async function delFavorito(conexion, data) {
+  try {
+    const call = "CALL delFav(?,?)";
+    const query = mysql.format(call, [data.idCliente, data.IdProducto]);
+    await conexion.query(query);
+  } catch (err) {
+    console.log("Ha ocurrido un error al ejecutar el query: ", err);
+    throw err;
+  }
 }
 
+export async function ProductFavoritosbyId(conexion, data) {
+  try {
+    const call = "CALL getFavoritosProductosCliente(?)";
+    const query = mysql.format(call, data.ids);
+    const [rows, fieds] = await conexion.query(query);
+    return rows;
+  } catch (err) {
+    console.log("Ha ocurrido un error al ejecutar el query: ", err);
+    throw err;
+  }
+}
 
-export async function ProductFavoritosbyId(conexion,data){
-    try{
-       const call='getFavoritosProductosCliente(?)'
-        const query=mysql.format(call,data.ids)
-    const [rows,fieds]= await conexion.query(query)
-    return rows
-    
-    }catch(err){
-        console.log("Ha ocurrido un error al ejecutar el query: ",err)
-        throw err;
-    }
-    }
-
-    export async function ServiceFavoritosbyId(conexion,data){
-        try{
-           const call='getFavoritosServiciosCliente(?)'
-            const query=mysql.format(call,data.ids)
-        const [rows,fieds]= await conexion.query(query)
-        return rows
-        
-        }catch(err){
-            console.log("Ha ocurrido un error al ejecutar el query: ",err)
-            throw err;
-        }
-        }
-    
+export async function ServiceFavoritosbyId(conexion, data) {
+  try {
+    const call = "CALL getFavoritosServiciosCliente(?)";
+    const query = mysql.format(call, data.ids);
+    const [rows, fieds] = await conexion.query(query);
+    return rows;
+  } catch (err) {
+    console.log("Ha ocurrido un error al ejecutar el query: ", err);
+    throw err;
+  }
+}
