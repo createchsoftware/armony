9eada5303515e87880d7882aa171459ccf36beff
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-18 23:41:17 -0700

    Corregido encabezado trasparente del Navbar

--- client/src/pages/spa/passwordResetVerificacion.jsx
+++ client/src/pages/spa/passwordResetVerificacion.jsx
@@ -1,12 +1,12 @@
-import Navbar from "../../components/ui/Navbar.jsx";
+import LayoutSecundario from "../../layouts/LayoutSecundario.jsx";
 import Verificacion from "../../components/ui/Login/Procesos/Contraseña/Verificacion.jsx";
-import Footer from "../../components/ui/FooterSecundario.jsx";
+
 const passwordResetVerificacion = () => {
   return (
     <>
-      <Navbar />
-      <Verificacion />
-      <Footer />
+      <LayoutSecundario>
+        <Verificacion />
+      </LayoutSecundario>
     </>
   );
 };
