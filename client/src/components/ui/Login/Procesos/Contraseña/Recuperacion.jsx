3a4f902f6c3ae24f6e746df1af68bf117d8c745c
KarimSoto <karimtec2003@gmail.com>
2024-05-20 11:32:25 -0700

    commit

--- client/src/components/ui/Login/Procesos/Contraseña/Recuperacion.jsx
+++ client/src/components/ui/Login/Procesos/Contraseña/Recuperacion.jsx
@@ -220,14 +220,14 @@ const Recuperacion = () => {
           </a>
           <div className="grid grid-cols-2 my-auto">
             <div className="grid place-content-start ml-8">
-              <a href="/spa">
+              
                 <button
                   aria-label="Cancelar"
                   className="bg-white text-rose-400 md:text-large lg:text-xl rounded-full px-4 py-2 mx-auto hover:bg-red-50 ring-2 ring-rose-400"
                 >
                   Cancelar
                 </button>
-              </a>
+              
             </div>
             <div className="grid place-content-end mr-8">
                 <button
