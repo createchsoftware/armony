4b6ad9423810a15ee46b44253e15bbd3b130f3e1
Jorge Lopez <157444533+JorgeInge@users.noreply.github.com>
2024-05-21 13:22:01 -0700

    no cambios 1

--- server/db/query/queryFavoritos.js
+++ server/db/query/queryFavoritos.js
@@ -3,17 +3,9 @@ import { endConnection } from "../connection.js";
 
 export async function addfavorito(conexion, data) {
   try {
-<<<<<<< HEAD
     const call = "CALL addFavorito(?,?,?)";
     const query = mysql.format(call, [data.idCliente, data.IdProducto,1]);
     await conexion.query(query);
-=======
-    const call = "CALL addFavorito(?,?,?)"; // Procedimiento almacenado de la base de datos
-    const query = mysql.format(call, [data.idCliente, data.IdProducto]); // Parametros necesarios para el procedimiento
-    const [rows, fields] = await conexion.query(query); // Ejecutamos query y almacenamos los valores resultantes
-    endConnection(); // Cerramos la conexion con la base de datos
-    return rows; // Retornamos los valores obtenidos en base al query
->>>>>>> 2aa6863a7bc01d04eb711ff959511be5625ee4de
   } catch (err) {
     console.log("Ha ocurrido un error al ejecutar el query: ", err);
     throw err;
