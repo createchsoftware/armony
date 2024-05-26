19bfbb0595e2742d8040d7b92c5f97e1fab09ca5
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-26 00:13:13 -0700

    Hechas modificaciones para poder manipular datos de agendar

--- server/routers/productos.js
+++ server/routers/productos.js
@@ -191,9 +191,7 @@ routerProductos.get("/serviciosFav", async (req, res) => {
     const resultado = await serviciosFav(conexion, {
       idCliente: req.body.idCliente,
     });
-    res
-      .status(200)
-      .json({ message: "Los servicios favoritos son: ", data: resultado });
+    res.status(202).json(resultado);
   } catch (err) {
     console.error("Ha ocurrido un error: ", err);
     res.status(500).send("Ha ocurrido un error al procesar tu solicitud");
@@ -261,9 +259,7 @@ routerProductos.get("/descuento", async (req, res) => {
 routerProductos.get("/servicios/descuento", async (req, res) => {
   try {
     const resultado = await serviciosDescuento(conexion);
-    res
-      .status(200)
-      .json({ message: "Servicios con descuento: ", data: resultado });
+    res.status(202).json(resultado);
   } catch (err) {
     console.error("Ha ocurrido un error: ", err);
     res.status(500).send("Ha ocurrido un error al procesar tu solicitud");
