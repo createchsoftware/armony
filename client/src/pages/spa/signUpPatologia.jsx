19b8097122e64acf0dea73d867afc192de3a9687
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-26 18:32:49 -0700

    Se añadió una validación para que no se acceda si está logueado

--- client/src/pages/spa/signUpPatologia.jsx
+++ client/src/pages/spa/signUpPatologia.jsx
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
 import LayoutSecundario from "../../layouts/LayoutSecundario.jsx";
 import Patologias from "../../components/ui/Login/Procesos/CrearCuenta/Patologias.jsx";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const passwordReset = () => {
+  async function checkLogin() {
+    let respuestaJson = null;
+    try {
+      const respuesta = await fetch("/api/logueado", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      respuestaJson = await respuesta.json();
+
+      if (respuestaJson.logueado == true) {
+        window.location.href = "/spa";
+      }
+    } catch (error) {}
+  }
+
+  useEffect(() => checkLogin(), []);
   return (
     <>
       <HelmetProvider>
