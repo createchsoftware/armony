1feabccc3fd7dc62d70fd9290120c55848c1d7eb
KrisG012 <a21490554@itmexicali.edu.mx>
2024-05-27 00:14:15 -0700

    Primeros avances en Agregar Saldo

--- client/src/pages/perfil/Agregarsaldo.jsx
+++ client/src/pages/perfil/Agregarsaldo.jsx
@@ -1,16 +1,8 @@
-import { Fragment, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import TarjetasPago from '../../components/ui/Tarjetas_de_pago';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    fa1,
-    fa2,
-    fa3,
-    fa4,
-    faCircle,
-    faAngleLeft,
-} from "@fortawesome/free-solid-svg-icons";
-
-
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import Navbar from '../../components/ui/Navbar'
 
 function AgregarSaldo(){
     const [sTarjeta, setsTarjeta] = useState({});
@@ -117,41 +109,52 @@ function AgregarSaldo(){
 
     }
 
-
-
     return (
-        
         <>
-        <div>
-            Regarda del Monedero
-        </div>
-        <label for="monto">Monto de Recarga</label>
-        <input type="text" name='monto' id='monto' placeholder='0.0' onChange={obtenerValorEnTiempoReal}/>
-        <div>
-            Metodos de pago:
-        </div>
-        {
-            array_toShow.map(objeto=>{
+            <Navbar />
+            <div className='grid w-1/2 rounded-2xl p-1 m-auto shadow-lg mt-24 border-2 border-[#036C65]'>
+                <div className=''>
+                    <a
+                        className="w-full text-sm items-center lg:text-base ml-5 justify-self-start relative cursor-pointer before:bg-black before:absolute before:-bottom-1 before:block before:h-[1px] before:w-full before:origin-bottom-right before:scale-x-0 before:transition before:duration-300 before:ease-in-out hover:before:origin-bottom-left hover:before:scale-x-100 hover:font-bold"
+                        aria-label="Volver"
+                        >
+                        <FontAwesomeIcon
+                            style={{ fontSize: "22px" }}
+                            icon={faAngleLeft}
+                        />{" "}
+                        Volver
+                    </a>
+                </div>
+                <div className='grid p-4'>
+                    <h1 className='text-3xl justify-self-center my-2'>Recarga monedero</h1>
+                    <img className='w-24 h-auto justify-self-center' src="../../../pictures/logoArmony.png" alt="" />
+                    <label htmlFor="monto" className='my-6 text-2xl justify-self-center'>Monto de Recarga:</label>
+                    <input type="text" name='monto' id='monto' placeholder='0.0' className='bg-[#036C65] text-white text-2xl text-center w-full rounded-2xl' onChange={obtenerValorEnTiempoReal}/>
+                    <h1 className='text-2xl my-4'>Metodos de pago:</h1>
+                    <div className='rounded-xl shadow-md'>
+                        <div className='h-5 bg-[#036C65] rounded-t-xl' />
+                        <div className='p-3'>
+                            {
+                                array_toShow.map(objeto=>{
 
-                if(objeto.predeterminada == 0){
-                    return <TarjetasPago tarjetas={objeto} funcion={seleccionarTarjeta} texto_btn={'seleccionar'}/>
-                }
-                else{
-                    return <TarjetasPago tarjetas={objeto} /> // no le mandamos la funcion
-                }
-            })
-        }
-        <button onClick={handleSubmit}>
-            Confirmar
-        </button>
+                                    if(objeto.predeterminada == 0){
+                                        return <TarjetasPago tarjetas={objeto} funcion={seleccionarTarjeta} texto_btn={'seleccionar'}/>
+                                    }
+                                    else{
+                                        return <TarjetasPago tarjetas={objeto} /> // no le mandamos la funcion
+                                    }
+                                })
+                            }
+                        </div>
+                    </div>
+                    <button onClick={handleSubmit}>
+                        Confirmar
+                    </button>
+                </div>
+            </div>
         </>
     );
 }
-
-
-
-
-
 // function AgregarSaldo() {
 //     return(
 //         <Elements stripe={stripePromise}>
